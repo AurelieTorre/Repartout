2b4fb34f9da888c7b993f18a0677074bcf338e15
AurelieTorre <aurelie.torre@laposte.net>
2024-06-12 10:35:13 +0200

    scroll navbar + links navbar + small circle colored

---
+++ script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // navbar management
+
+    const header = document.getElementById('header');
+    let lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.scrollY;
+        // || document.documentElement.scrollTop;
+
+        if (scrollTop > lastScrollTop) {
+            // Scroll down
+            header.style.top = '-15vh';
+        } else {
+            // Scroll up
+            header.style.top = '0';
+        }
+        lastScrollTop = scrollTop;
+    });
+
+    // small circles management
+
+    const circles = document.querySelectorAll(".small-circle");
+    const leftArrow = document.getElementById("left-arrow");
+    const rightArrow = document.getElementById("right-arrow");
+    const firstCircle = circles[0];
+
+    // colors the first circle when one land on the page
+    firstCircle.classList.add("active");
+    firstCircle.style.background = "#06D6A0";
+
+    circles.forEach(circle => {
+        circle.addEventListener("click", function() {
+            // Remove the active class from any currently active circle
+            const activeCircle = document.querySelector(".small-circle.active");
+            if (activeCircle) {
+                activeCircle.classList.remove("active");
+                activeCircle.style.background = "#112A46";
+            }
+
+            // Add the active class to the clicked circle
+            this.classList.add("active");
+            this.style.background = "#06D6A0";
+        });
+    });
+
+    leftArrow.addEventListener("click", function() {
+        const activeCircle = document.querySelector(".small-circle.active");
+        if (activeCircle) {
+            const prevCircle = activeCircle.previousElementSibling;
+            if (prevCircle && prevCircle.classList.contains("small-circle")) {
+                activeCircle.classList.remove("active");
+                activeCircle.style.background = "#112A46";
+                prevCircle.classList.add("active");
+                prevCircle.style.background = "#06D6A0";
+            }
+        }
+    });
+
+    rightArrow.addEventListener("click", function() {
+        const activeCircle = document.querySelector(".small-circle.active");
+        if (activeCircle) {
+            const nextCircle = activeCircle.nextElementSibling;
+            if (nextCircle && nextCircle.classList.contains("small-circle")) {
+                activeCircle.classList.remove("active");
+                activeCircle.style.background = "#112A46";
+                nextCircle.classList.add("active");
+                nextCircle.style.background = "#06D6A0";
+            }
+        }
+    });
+});
